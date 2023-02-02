a63cd7a78865462119dcc050adbc124d4b48bba0
fangdajiang <fang.dajiang@gmail.com>
2023-02-02 18:00:53 +0800

    init

---
+++ src/main/java/org/tlbc/worship/WorshipCoursewareMakingApplication.java
@@ -0,0 +1,13 @@
+package org.tlbc.worship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorshipCoursewareMakingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorshipCoursewareMakingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tlbc/worship/coursewaremaking/WorshipCoursewareMakingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tlbc.worship.coursewaremaking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorshipCoursewareMakingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
